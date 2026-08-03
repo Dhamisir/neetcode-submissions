54ee0833376f88eb55c158eb7d5135f3fecbc629
Himanshu Dhami <96005514+Dhamisir@users.noreply.github.com>
2026-08-03 14:22:10 +0530

    Add: move-zeroes - submission-2

---
+++ Data Structures & Algorithms/move-zeroes/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    moveZeroes(nums) {
+        let i = 0;
+        let j = 1;
+        while (j < nums.length) {
+            if (nums[i] != 0) {
+                i++;
+            } else if (nums[j] != 0) {
+                let temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+            j++;
+        }
+    }
+}
